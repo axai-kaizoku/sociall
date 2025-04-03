bee64b3ad3d797d56346dbe5f717552f90ba3adc
Akshay <02b3akshay@gmail.com>
2025-04-04 00:10:58 +0530

    ***********finally got the correct RAW query to get count with hashtags

--- src/components/common/trends-sidebar.tsx
+++ src/components/common/trends-sidebar.tsx
@@ -5,12 +5,20 @@ import Link from "next/link"
 import { Suspense } from "react"
 import { UserAvatar } from "./user/user-avatar"
 import { Button } from "../ui/button"
+import { unstable_cache } from "next/cache"
+import { postTable } from "@/server/db/schema"
+import { sql } from "drizzle-orm"
+import { formatNumber } from "@/lib/utils"
 
 export const TrendsSidebar = () => {
   return (
     <div className="sticky top-[5.25rem] hidden md:block lg:w-80 w-72 h-fit flex-none space-y-5">
       <Suspense fallback={<Loader2 className="mx-auto animate-spin" />}>
         <WhoToFollow />
+      </Suspense>
+
+      <Suspense fallback={<Loader2 className="mx-auto animate-spin" />}>
+        <TrendingTopics />
       </Suspense>
     </div>
   )
@@ -59,6 +67,81 @@ async function WhoToFollow() {
           <Button>Follow</Button>
         </div>
       ))}
+    </div>
+  )
+}
+
+const getTrendingTopics = unstable_cache(
+  async () => {
+    // const result = await db
+    //   .select({
+    //     data: sql<{ hashtag: string; count: bigint }[]>`
+    //   SELECT unnest(regexp_matches(content, '#[a-zA-Z0-9_]+', 'g')) AS hashtag, COUNT(*) AS count
+    //   FROM ${postTable}
+    //   GROUP BY (hashtag)
+    //   ORDER BY count DESC, hashtag ASC
+    //   LIMIT 5
+    // `,
+    //   })
+    //   .from(postTable)
+
+    // const result = await db
+    //   .select({
+    //     postContent: postTable.content,
+    //     hashtag: sql`${postTable.content} REGEXP '^/#/+`,
+    //     // sql`${files.path} REGEXP '^/tmp/foo(5)/tutu`
+    //     // createdAt: postTable.createdAt,
+    //     count: sql<number>`count(${postTable.content})`.mapWith(Number),
+    //   })
+    //   .from(postTable)
+    //   .groupBy(sql`${postTable.content}`)
+
+    const res1 = await db.execute(
+      sql<{ hashtag: string }[]>`
+          SELECT unnest(regexp_matches(content, '#[a-zA-Z0-9_]+', 'g')) AS hashtag, count(*) AS count
+          FROM ${postTable}
+          GROUP BY (hashtag)
+          ORDER BY count DESC, hashtag ASC
+          LIMIT 5
+        `
+    )
+
+    console.log(res1)
+    // .orderBy(sql`${postTable.createdAt} desc nulls first`)
+
+    // return result[0]?.data.map((row) => ({
+    //   hashtag: row.hashtag,
+    //   count: Number(row.count),
+    // }))
+    // console.log(result)
+  },
+  ["trending_topics"],
+  { revalidate: 3 * 60 * 60 }
+)
+
+async function TrendingTopics() {
+  const trendingTopics = await getTrendingTopics()
+  return (
+    <div className="space-y-5 rounded-2xl bg-card p-5 shadow-sm">
+      <div className="text-xl font-bold">
+        {trendingTopics?.map(({ hashtag, count }) => {
+          const title = hashtag.split("#")[1]
+
+          return (
+            <Link key={title} href={`/hashtag/${title}`} className="block">
+              <p
+                className="line-clamp-1 break-all font-semibold hover:underline"
+                title={hashtag}
+              >
+                {hashtag}
+              </p>
+              <p className="text-sm text-muted-foreground">
+                {formatNumber(count)} {count === 1 ? "post" : "posts"}
+              </p>
+            </Link>
+          )
+        })}
+      </div>
     </div>
   )
 }
