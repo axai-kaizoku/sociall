74aef6535f402cbfd8cad467e3aeed7fee4a3b60
Akshay Yelle <02b3akshay@gmail.com>
2025-04-27 19:04:58 +0530

    add posts attachments in db

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,6 +1,6 @@
 import { validateRequest } from "@/server/auth"
 import { db } from "@/server/db"
-import { userTable } from "@/server/db/schema"
+import { mediaTable, userTable } from "@/server/db/schema"
 import { eq } from "drizzle-orm"
 import { createUploadthing, type FileRouter } from "uploadthing/next"
 import { UploadThingError, UTApi } from "uploadthing/server"
@@ -35,6 +35,28 @@ export const fileRouter = {
         .where(eq(userTable.id, metadata.user.id))
 
       return { avatarUrl: newAvatarUrl }
+    }),
+  attachment: f({
+    image: { maxFileSize: "4MB", maxFileCount: 5 },
+    video: { maxFileSize: "64MB", maxFileCount: 5 },
+  })
+    .middleware(async () => {
+      const { user } = await validateRequest()
+
+      if (!user) throw new UploadThingError("Unauthorized") as Error
+
+      return {}
+    })
+    .onUploadComplete(async ({ file }) => {
+      const [media] = await db
+        .insert(mediaTable)
+        .values({
+          url: file.ufsUrl,
+          type: file.type.startsWith("image") ? "IMAGE" : "VIDEO",
+        })
+        .returning()
+
+      return { mediaId: media?.id }
     }),
 } satisfies FileRouter
 
